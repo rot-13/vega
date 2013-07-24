753ea3bfcfb1721241b3d41ab07c8c0cec93f699
Gilad Goldberg <giladgo@gmail.com>
2013-07-24 17:01:59 +0300

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Vega::Application.config.secret_token = '20f5642682b8554cb232ca5d199a62aa1f9507c5e1eb779a7609b10d40e7b2ec23ac4f9894c3bfa75d709401feae3dd24fc164187aa9058e8effbe2ac23efb2c'
